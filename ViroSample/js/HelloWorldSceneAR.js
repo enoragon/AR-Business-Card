2c03d2408cc28111c7aa8d642b84ae75e7f4f646
enoch <enoch@selfinteractive.com>
2018-10-21 22:33:46 -0500

    Reset code to starting point

--- ViroSample/js/HelloWorldSceneAR.js
+++ ViroSample/js/HelloWorldSceneAR.js
@@ -31,9 +31,6 @@ export default class HelloWorldSceneAR extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroARImageMarker target={"card"}>
-          <ViroBox position={[0, .25, 0]} scale={[.2, .2, .2]} />
-        </ViroARImageMarker>
       </ViroARScene>
     );
   }
@@ -41,21 +38,13 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Welcome to TechItOut"
+        text : "Hello World!"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
 }
-
-ViroARTrackingTargets.createTargets({
-  "card": {
-    source: require('./res/tio.png'),
-    orientation: 'Up',
-    physicalWidth: 0.1//meters
-  }
-})
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
